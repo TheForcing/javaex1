2e689ac7cb2f84899e28a2589ba9a508b41fb874
asz <asz@DESKTOP-E4K5UO9>
2021-07-26 16:14:46 +0900

    Java Lang api obj

--- JavaEX/src/com/javaex/api/objectclass/v1/LangClassTest.java
+++ JavaEX/src/com/javaex/api/objectclass/v1/LangClassTest.java
@@ -9,7 +9,7 @@ public class LangClassTest {
 	    System.out.println("p와 p2는 같은 객체인가?"+ (p==p2));
 	    // == 은 참조 주소 비교
 	    // .equals() 내부 값의 비교
-	    System.out.println("p와p2는 같은 값을 가졌는가"+p.equals(p2));
+	    System.out.println("p와p2는 같은 값을 가졌는가?"+p.equals(p2));
 	     
 	}
 
