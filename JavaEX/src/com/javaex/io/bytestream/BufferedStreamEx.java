ce81795c726334bb11d29c2ec1f3919556118833
asz <asz@DESKTOP-E4K5UO9>
2021-07-30 10:47:11 +0900

    # WARNING: head commit changed in the meantime
    list, stack,queue 자료형

---
+++ JavaEX/src/com/javaex/io/bytestream/BufferedStreamEx.java
@@ -0,0 +1,70 @@
+package com.javaex.io.bytestream;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class BufferedStreamEx {
+	
+	 static final String rootPath= System.getProperty("user.dir")+"\\files\\";
+	 static String filename= rootPath + "char.txt";
+	 
+	
+	
+	static String source= rootPath + "img.jpg";
+	static String target= rootPath + "img_bufferd_copy.jpg";
+	
+	public static void main(String[] args) {
+		InputStream fis= null;
+		OutputStream fos= null;
+		BufferedInputStream bis= null;
+		BufferedOutputStream bos= null;
+		try { 
+			 fis = new FileInputStream(source); // 주스트림
+			 fos= new FileOutputStream(target); //주스트림
+		 
+			//버퍼 기반 보조 스트림
+			 bis= new BufferedInputStream(fis);//보조스트림
+			 bos= new BufferedOutputStream(fos);//출력
+		
+		   //int data= 0; 
+		   byte[] data= new byte[10240]; //버퍼
+		   int size= 0; // 읽은 사이즈
+		   
+		   while((size =bis.read(data))!=-1) { //버퍼에 읽어드릴 데이터가 없을떄 까지
+			   bos.write(data,0, size); // 버퍼 내용을 출력
+			   System.out.println(size+"바이트 복사!");
+			   
+		   }
+		 
+	
+		System.out.println("파일 복사 완료!");
+		
+		 
+	}catch (FileNotFoundException e) {
+		e.printStackTrace();
+		
+	}catch (IOException e) {
+		e.printStackTrace();
+	} finally {
+		try {
+			bos.close();  //연결된 주수트림도 닫힌다.
+			
+			bis.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		}
+	
+	
+
+	}
+}
+
+
