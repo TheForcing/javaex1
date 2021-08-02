33f255106065bcc9f890b68993e111de5fd3d5b6
BIT <BIT@DESKTOP-2HGB39O>
2021-08-02 17:53:05 +0900

    미니 프로젝트

---
+++ JavaEX/src/miniproject/NumberMenu.java
@@ -0,0 +1,23 @@
+package miniproject;
+
+import java.util.Scanner;
+
+public class NumberMenu {
+
+	public static void main(String[] args) {
+		
+
+		Scanner scr= new Scanner(System.in);
+		String name;
+		int tel;
+		int hp;
+		
+		  System.out.println("*****************************");
+		  System.out.println("*    전화번호 관리 프로그램    *");
+		  System.out.println("-----------------------------");
+		  System.out.println("메뉴번호:");
+		  int menuNo=  scr.nextInt();
+		
+	}
+
+}
